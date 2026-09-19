f5e026374427df985c5bb44e002709b54e796251
dwi5h <dwish.rezpector@gmail.com>
2026-09-19 11:41:09 +0700

    Add account entity and controller

---
+++ src/main/java/com/dwish/minitrxsettlement/controller/AccountController.java
@@ -0,0 +1,12 @@
+package com.dwish.minitrxsettlement.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/account")
+@RequiredArgsConstructor
+public class AccountController {
+}
---
+++ src/main/java/com/dwish/minitrxsettlement/entity/Account.java
@@ -0,0 +1,30 @@
+package com.dwish.minitrxsettlement.entity;
+
+import lombok.AccessLevel;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Table(name = "account")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String name;
+
+    private String cif;
+
+    private BigDecimal balance;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+}
