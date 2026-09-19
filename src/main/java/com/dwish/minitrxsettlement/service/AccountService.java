4af979ed3509f5a808a6aa3964e6c8375c95e926
dwi5h <dwish.rezpector@gmail.com>
2026-09-19 13:52:26 +0700

    Add getAllAccounts and getAccountById method

--- src/main/java/com/dwish/minitrxsettlement/service/AccountService.java
+++ src/main/java/com/dwish/minitrxsettlement/service/AccountService.java
@@ -1,4 +1,39 @@
 package com.dwish.minitrxsettlement.service;
 
+import com.dwish.minitrxsettlement.dto.AccountPaggingRequest;
+import com.dwish.minitrxsettlement.dto.AccountResponse;
+import com.dwish.minitrxsettlement.exception.ResourceNotFoundException;
+import com.dwish.minitrxsettlement.mapper.AccountMapper;
+import com.dwish.minitrxsettlement.repository.AccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
 public class AccountService {
+
+    private final AccountRepository accountRepository;
+
+    public Page<AccountResponse> getAllAccounts(AccountPaggingRequest request) {
+        String sortBy = "createdAt";
+        Sort sort = request.direction().equalsIgnoreCase(Sort.Direction.DESC.name())
+                ? Sort.by(sortBy).descending()
+                : Sort.by(sortBy).ascending();
+
+        Pageable pageable = PageRequest.of(request.page(), request.size(), sort);
+
+        return accountRepository.findAll(pageable)
+                .map(AccountMapper::toResponse);
+    }
+
+    public AccountResponse getAccountById(UUID id) {
+        return accountRepository.findById(id)
+                .map(AccountMapper::toResponse)
+                .orElseThrow(() -> new ResourceNotFoundException("Produk dengan ID " + id + " tidak ditemukan"));
+    }
 }
