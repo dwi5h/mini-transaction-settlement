9881f73879a53dabb4a0c0efefd2cf5d73baaf0e
dwi5h <dwish.rezpector@gmail.com>
2026-09-19 13:54:07 +0700

    Add getProductById and getAllProducts method in controller

--- src/main/java/com/dwish/minitrxsettlement/controller/AccountController.java
+++ src/main/java/com/dwish/minitrxsettlement/controller/AccountController.java
@@ -1,12 +1,42 @@
 package com.dwish.minitrxsettlement.controller;
 
 
+import com.dwish.minitrxsettlement.dto.AccountPaggingRequest;
+import com.dwish.minitrxsettlement.dto.AccountResponse;
+import com.dwish.minitrxsettlement.service.AccountService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/account")
 @RequiredArgsConstructor
 public class AccountController {
+
+    private static final Logger log = LoggerFactory.getLogger(AccountController.class);
+    private final AccountService accountService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AccountResponse> getAccountById(@PathVariable UUID id) {
+        log.info("REST request untuk mendapatkan Account berdasarkan ID: {}", id);
+
+        AccountResponse response = accountService.getAccountById(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<AccountResponse>> getAllAccounts(@Valid AccountPaggingRequest request) {
+        log.info("REST request untuk mendapatkan semua produk. Halaman: {}, Ukuran: {}, ({})",
+                request.page(), request.size(), request.direction());
+
+        Page<AccountResponse> accounts = accountService.getAllAccounts(request);
+
+        return ResponseEntity.ok(accounts);
+    }
 }
