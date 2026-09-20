0fcb9eb0637977e2ee010abe564bc86c119f4287
dwi5h <dwish.rezpector@gmail.com>
2026-09-20 11:10:30 +0700

    Change ListCrudRepository into JpaRepository

--- src/main/java/com/dwish/minitrxsettlement/repository/TransactionRepository.java
+++ src/main/java/com/dwish/minitrxsettlement/repository/TransactionRepository.java
@@ -1,11 +1,12 @@
 package com.dwish.minitrxsettlement.repository;
 
 import com.dwish.minitrxsettlement.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface TransactionRepository extends ListCrudRepository<Transaction, UUID> {
+public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
 }
