973b520065a1021e5c96497697baccb7d317cf84
dwi5h <dwish.rezpector@gmail.com>
2026-09-26 21:45:55 +0700

    Add regex pattern for valid input of accountId and type field

--- src/main/java/com/dwish/minitrxsettlement/dto/TransactionCreateRequest.java
+++ src/main/java/com/dwish/minitrxsettlement/dto/TransactionCreateRequest.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 
 public record TransactionCreateRequest(
         @NotBlank(message = "Account Id tidak boleh kosong")
+        @Pattern(regexp = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$", message = "Account Id harus bernilai format id valid")
         String accountId,
 
         @NotNull(message = "Amount tidak boleh kosong")
@@ -14,5 +15,6 @@ public record TransactionCreateRequest(
         BigDecimal amount,
 
         @NotBlank(message = "Type transaksi tidak boleh kosong")
+        @Pattern(regexp = "^(?i)(DEBIT|CREDIT)$", message = "Type transaksi harus bernilai 'DEBIT' atau 'CREDIT'")
         String type
 ) {}
