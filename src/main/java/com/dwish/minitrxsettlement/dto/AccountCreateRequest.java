d38d748209e4c1cb4c9ad8efd7143b1279b8b246
dwi5h <dwish.rezpector@gmail.com>
2026-09-26 21:45:08 +0700

    Add initialBalance with @Positive annotation

--- src/main/java/com/dwish/minitrxsettlement/dto/AccountCreateRequest.java
+++ src/main/java/com/dwish/minitrxsettlement/dto/AccountCreateRequest.java
@@ -1,10 +1,16 @@
 package com.dwish.minitrxsettlement.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
 
 public record AccountCreateRequest(
         @NotBlank(message = "Name tidak boleh kosong")
         String name,
+
+        @Positive(message = "Initial Balance harus lebih besar dari 0")
+        BigDecimal initialBalance,
 
         @NotBlank(message = "CIF tidak boleh kosong")
         String cif
